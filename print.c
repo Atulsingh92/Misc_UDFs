ea616b648e3b608c484d5398bf94597f3d384a0c
Atul Singh <30570817+Atulsingh92@users.noreply.github.com>
2021-02-18 10:14:09 +0000

    outputs static p, mdot and area.

--- print.c
+++ print.c
@@ -7,7 +7,7 @@
   Domain *domain;
   real flow=0.;
   real p=0.;
-
+  
  void Print_Thread_Face_centroids(Domain *domain, int id)
  /*Prints centroids of the chosen surface"*/
  {
@@ -26,21 +26,42 @@
 
  void Print_Pressure_mdot(Domain *domain, int id)
  {
-
     cell_t c;
     Thread *t;
     face_t f;
+    real flow=0.0;
+    real p=0.0;
+    real area[ND_ND];
+    real total_area = 0.0;
+    real p_0 = RP_Get_Float("operating-pressure");
+    real resultp = 0.0;
+    real Amag;
+    real pglob = 0.0;
+    real areaglob = 0.0;
+    real total_p;
+
     t = Lookup_Thread(domain, id);
     begin_f_loop(f,t)
     {
       flow += F_FLUX(f,t); // prints below the given fprintf command
-      p += F_P(f,t); //write area aveg pressure, tallies with area averaged total pressure, but not always, mostly 0.
+      //p += F_P(f,t); //write area aveg pressure, tallies with area averaged total pressure, but not always, mostly 0.
+      F_AREA(area,f,t);
+      Amag = NV_MAG(area);
+      total_area += Amag;
+      p = C_P(f,t);
+      total_p += p * Amag; //NV_MAG(area);
     }
     end_f_loop(f,t)
-    //p = C_P(c,t);
+    
+    pglob = PRF_GRSUM1(total_p);
+    areaglob = PRF_GRSUM1(total_area);
+    
+    resultp  = pglob / areaglob;
+    
     fprintf(fout, "for zoneid = %d \n", id);
-    fprintf(fout, "pressure = %g \n", p);
-    fprintf(fout, "mass Flow Rate: %g \n", flow);
+    fprintf(fout, "pressure = %g \n", resultp);
+    fprintf(fout, "mass Flow Rate = %g \n", flow);
+    fprintf(fout, "total area  = %g \n", total_area);
  }
 
  DEFINE_ON_DEMAND(get_pressure)
